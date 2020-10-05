52dce8b5f6784cc972d6d389824f7228ffdea515
Neel Trivedi <neeltrivedi@Neels-MacBook-Pro.local>
2020-10-04 21:52:16 -0400

    final changes to homework

--- run.py
+++ run.py
@@ -1,50 +1,50 @@
 import boto3
 import csv
 
-s3=boto3.resource('s3',aws_access_key_id='AKIAIS6XWNDVA4CAJD5Q',aws_secret_access_key='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')
+s3=boto3.resource('s3',aws_access_key_id='',aws_secret_access_key='')
 
 #create bucket
-#s3.create_bucket(Bucket='neeldatatable',CreateBucketConfiguration={'LocationConstraint':'us-west-2'})
+s3.create_bucket(Bucket='neeldatatable',CreateBucketConfiguration={'LocationConstraint':'us-west-2'})
 
 #storing object in bucket
-#s3.Object('neeldatatable','adorable-puppy.jpg ').put(Body=open('/Users/neeltrivedi/code/cs1660/homework2/adorable-puppy.jpg','rb'))
+s3.Object('neeldatatable','adorable-puppy.jpg ').put(Body=open('/Users/neeltrivedi/code/cs1660/homework2/adorable-puppy.jpg','rb'))
 
 
 dyndb = boto3.resource('dynamodb',region_name='us-west-2')
 
 # # The first time that we define a table, we use
 
-# table=dyndb.create_table(
-#     TableName='DataTable',
-#     KeySchema=[
-#             {'AttributeName':'PartitionKey','KeyType':'HASH'},
-#             {'AttributeName':'RowKey','KeyType':'RANGE'}
-#     ] ,
-#     AttributeDefinitions=[
-#         { 'AttributeName': 'PartitionKey', 'AttributeType': 'S'},
-#         { 'AttributeName': 'RowKey',      'AttributeType': 'S'}
-#         # { 'AttributeName': 'date',        'AttributeType': 'S'},
-#         # { 'AttributeName': 'description', 'AttributeType': 'S'},
-#         # { 'AttributeName': 'url',         'AttributeType': 'S'}
-#     ],
-#     ProvisionedThroughput= {
-#         'ReadCapacityUnits': 5,
-#         'WriteCapacityUnits': 5 
-#     }
-# )
+table=dyndb.create_table(
+    TableName='DataTable',
+    KeySchema=[
+            {'AttributeName':'PartitionKey','KeyType':'HASH'},
+            {'AttributeName':'RowKey','KeyType':'RANGE'}
+    ] ,
+    AttributeDefinitions=[
+        { 'AttributeName': 'PartitionKey', 'AttributeType': 'S'},
+        { 'AttributeName': 'RowKey',      'AttributeType': 'S'}
+        # { 'AttributeName': 'date',        'AttributeType': 'S'},
+        # { 'AttributeName': 'description', 'AttributeType': 'S'},
+        # { 'AttributeName': 'url',         'AttributeType': 'S'}
+    ],
+    ProvisionedThroughput= {
+        'ReadCapacityUnits': 5,
+        'WriteCapacityUnits': 5 
+    }
+)
 
-# # Wait for the table to be created
+# Wait for the table to be created
 
-# table.meta.client.get_waiter('table_exists').wait(TableName='DataTable')
+table.meta.client.get_waiter('table_exists').wait(TableName='DataTable')
 
-# table.put_item(
-#     Item={
-#         'PartitionKey': 'S',
-#         'RowKey': 'S',
-#         'description': 'S',
-#         'url': 'S'
-#     }
-# )
+table.put_item(
+    Item={
+        'PartitionKey': 'S',
+        'RowKey': 'S',
+        'description': 'S',
+        'url': 'S'
+    }
+)
 
 #If the table has been previously defined thenuse
 
